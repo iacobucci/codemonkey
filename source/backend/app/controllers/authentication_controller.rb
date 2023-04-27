f0e1df5403156ca10651ffc32a043f7bedb8d239
iacobucci <iacobuccivalerio@gmail.com>
2023-04-28 01:20:04 +0200

    deleted:    architecture.txt 	new file:   backend/app/controllers/api/app_names_controller.rb 	new file:   backend/app/controllers/api/logins_controller.rb 	modified:   backend/app/controllers/application_controller.rb 	modified:   backend/app/controllers/authentication_controller.rb 	modified:   backend/app/models/login.rb 	modified:   backend/config/routes.rb 	new file:   backend/test/controllers/api/app_names_controller_test.rb 	new file:   backend/test/controllers/api/logins_controller_test.rb 	modified:   database/run.sh 	modified:   devel.sh 	modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.module.ts 	modified:   frontend/src/app/page1/page1.component.html 	modified:   frontend/src/app/page1/page1.component.ts 	modified:   frontend/src/app/page2/page2.component.html 	modified:   frontend/src/index.html 	modified:   ../todo.txt

--- source/backend/app/controllers/authentication_controller.rb
+++ source/backend/app/controllers/authentication_controller.rb
@@ -1,13 +1,13 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   
-  def create
-    login = Login.find_by_username(params[:username])
+  # def create
+  #   login = Login.find_by_username(params[:username])
     
-    if login&.authenticate(params[:password])
-      render json: { message: "Logged in successfully" }, status: :ok
-    else
-      render json: { error: "Invalid username or password" }, status: :unauthorized
-    end
-  end
+  #   if login&.authenticate(params[:password])
+  #     render json: { message: "Logged in successfully" }, status: :ok
+  #   else
+  #     render json: { error: "Invalid username or password" }, status: :unauthorized
+  #   end
+  # end
 end
--- source/backend/app/models/login.rb
+++ source/backend/app/models/login.rb
@@ -1,4 +1,7 @@
 class Login < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  has_secure_password
+  self.table_name = "codemonkey.logins"
+  self.primary_key = "username"
+  
+  validates :username, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { maximum: 255 }
 end
