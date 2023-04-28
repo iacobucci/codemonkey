def508abd9cebf10cd180f9552fd819a40b8faf3
iacobucci <iacobuccivalerio@gmail.com>
2023-04-28 20:14:27 +0200

    containerizzazione 	modified:   backend/Dockerfile 	modified:   backend/Gemfile 	modified:   backend/Gemfile.lock 	modified:   backend/config/credentials.yml.enc 	modified:   backend/config/database.yml 	modified:   backend/devel.sh 	new file:   build.sh 	new file:   database/test/logins.sql 	modified:   docker-compose.yml 	new file:   expose.sh 	modified:   nginx.conf 	modified:   run.sh 	modified:   ../todo.txt

--- source/backend/devel.sh
+++ source/backend/devel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-BACKEND_DATABASE_USER=admin BACKEND_DATABASE_PASSWORD=password rails server
+BACKEND_DATABASE_USER=admin BACKEND_DATABASE_PASSWORD=password RAILS_ENV=development rails server
